01af1b2b24e39e349905faf694d27f0817780c58
tunabaran <tunaurut@gmail.com>
2018-06-04 00:17:31 +0300

    Minor Bug fix.

--- JsonMapper.java
+++ JsonMapper.java
@@ -32,7 +32,7 @@ public abstract class JsonMapper {
 
             if(f.getType().getName().equals("com.android.tools.ir.runtime.IncrementalChange") ||
                     f.getName().equals("serialVersionUID")){
-                break;
+                continue;
             }
 
             Class typeClass;
